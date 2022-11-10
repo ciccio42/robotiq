968284b51a2e43078583c49ba9e15d22f3694d5d
ciccio42 <francescorosa97@virgilio.it>
2022-11-10 11:54:01 +0100

    Modified joint_state_publisher and robot_state_publisher

--- robotiq_2f_gripper_control/src/robotiq_2f_gripper_control/baseRobotiq2FGripper.py
+++ robotiq_2f_gripper_control/src/robotiq_2f_gripper_control/baseRobotiq2FGripper.py
@@ -55,33 +55,33 @@ class robotiqbaseRobotiq2FGripper:
     def verifyCommand(self, command):
         """Function to verify that the value of each variable satisfy its limits."""
     	   	
-   	#Verify that each variable is in its correct range
-   	command.rACT = max(0, command.rACT)
-   	command.rACT = min(1, command.rACT)
-   	
-   	command.rGTO = max(0, command.rGTO)
-   	command.rGTO = min(1, command.rGTO)
+        #Verify that each variable is in its correct range
+        command.rACT = max(0, command.rACT)
+        command.rACT = min(1, command.rACT)
+        
+        command.rGTO = max(0, command.rGTO)
+        command.rGTO = min(1, command.rGTO)
 
-   	command.rATR = max(0, command.rATR)
-   	command.rATR = min(1, command.rATR)
-   	
-   	command.rPR  = max(0,   command.rPR)
-   	command.rPR  = min(255, command.rPR)   	
+        command.rATR = max(0, command.rATR)
+        command.rATR = min(1, command.rATR)
+        
+        command.rPR  = max(0,   command.rPR)
+        command.rPR  = min(255, command.rPR)   	
 
-   	command.rSP  = max(0,   command.rSP)
-   	command.rSP  = min(255, command.rSP)   	
+        command.rSP  = max(0,   command.rSP)
+        command.rSP  = min(255, command.rSP)   	
 
-   	command.rFR  = max(0,   command.rFR)
-   	command.rFR  = min(255, command.rFR) 
-   	
-   	#Return the modified command
-   	return command
+        command.rFR  = max(0,   command.rFR)
+        command.rFR  = min(255, command.rFR) 
+        
+        #Return the modified command
+        return command
 
     def refreshCommand(self, command):
         """Function to update the command which will be sent during the next sendCommand() call."""
     
-	#Limit the value of each variable
-    	command = self.verifyCommand(command)
+	    #Limit the value of each variable
+        command = self.verifyCommand(command)
 
         #Initiate command as an empty list
         self.message = []
@@ -104,16 +104,16 @@ class robotiqbaseRobotiq2FGripper:
         """Request the status from the gripper and return it in the Robotiq2FGripper_robot_input msg type."""
 
         #Acquire status from the Gripper
-        status = self.client.getStatus(6);
+        status = self.client.getStatus(6)
 
         #Message to output
         message = inputMsg.Robotiq2FGripper_robot_input()
 
         #Assign the values to their respective variables
-        message.gACT = (status[0] >> 0) & 0x01;        
-        message.gGTO = (status[0] >> 3) & 0x01;
-        message.gSTA = (status[0] >> 4) & 0x03;
-        message.gOBJ = (status[0] >> 6) & 0x03;
+        message.gACT = (status[0] >> 0) & 0x01        
+        message.gGTO = (status[0] >> 3) & 0x01
+        message.gSTA = (status[0] >> 4) & 0x03
+        message.gOBJ = (status[0] >> 6) & 0x03
         message.gFLT =  status[2]
         message.gPR  =  status[3]
         message.gPO  =  status[4]
